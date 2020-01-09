d09be747f143535397c6af38b0ca9cf3a3e8e555
dongzwei123 <56243704+dongzwei123@users.noreply.github.com>
2020-01-09 20:59:07 +0900

    Updates

---
+++ precache-manifest.4bf7678758c99dcb83e3f6827939d012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60565a9ee6af6ac2c4cf63e1d4ea34ef",
+    "url": "/dongzwei123/movie_app_2019/index.html"
+  },
+  {
+    "revision": "28990052eeb1e01e5917",
+    "url": "/dongzwei123/movie_app_2019/static/css/main.f3538baa.chunk.css"
+  },
+  {
+    "revision": "0de2173c7f51200267b9",
+    "url": "/dongzwei123/movie_app_2019/static/js/2.bd04fb22.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/dongzwei123/movie_app_2019/static/js/2.bd04fb22.chunk.js.LICENSE"
+  },
+  {
+    "revision": "28990052eeb1e01e5917",
+    "url": "/dongzwei123/movie_app_2019/static/js/main.4e9470b2.chunk.js"
+  },
+  {
+    "revision": "9688731a1da12b2fe8c3",
+    "url": "/dongzwei123/movie_app_2019/static/js/runtime-main.62cf6404.js"
+  }
+]);
